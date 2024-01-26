4f40dc1076819eded34883d13600f3fd3d530b4a
Chikiryava <alex.man20151@gmail.com>
2024-01-26 22:31:01 +0500

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+static bool IsEvan(int number) {
+    return number % 2 == 0;
+}
+
+int main()
+{
+    std::cout << "Enter the number\n";
+    int a;
+    std::cin >> a;
+    std::cout << "Number " << a << " is ";
+    if (IsEvan(a)) {
+        std::cout << "evan" << std::endl;
+    }
+    else {
+        std::cout << "odd" << std::endl;
+    }
+}
+
+
+
